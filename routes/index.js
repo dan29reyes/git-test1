f08d05558b54cecc0e606c3ee891625dedf07690
dan29reyes <dan29reyes@unitec.edu>
2024-01-29 18:36:58 -0600

    axios y weather

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/envtest', function(req, res, next){
-  res.send({
-    env: process.env.WEATHER_API
-  })
-})
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+require("dotenv").config();
+const axios = require("axios");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send("hola");
+router.get("/", function (req, res, next) {
+  res.send({
+    env: process.env.WEATHER_API,
+  });
+});
+
+router.get("/envtest", async function (req, res, next) {
+  let options = {
+    method: "GET",
+    url:
+      "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=" +
+      process.env.WEATHER_API,
+  };
+  const response = await axios.request(options);
+  res.send({
+    env: process.env.WEATHER_API,
+    response: response.data,
+  });
 });
 
 module.exports = router;
