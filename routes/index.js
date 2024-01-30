e016ad5bcb0365cd820820de9152a53141795291
dan29reyes <dan29reyes@unitec.edu>
2024-01-29 18:04:21 -0600

    remove node modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/envtest', function(req, res, next){
+  res.send({
+    env: process.env.WEATHER_API
+  })
+})
 
 module.exports = router;
