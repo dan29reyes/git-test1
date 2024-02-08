89abff0822e63db554c26592e266f452f2a837b3
dan29reyes <dan29reyes@unitec.edu>
2024-02-07 18:47:28 -0600

    agregar a estante

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -1,61 +1,6 @@
 const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_CONNECTION);
 
-async function createTables(req, res) {
-  try {
-    db.createCollection("Libros", {
-      $jsonSchema: {
-        bsonType: "object",
-        additionalProperties: false,
-        required: [
-          "_id",
-          "nombre",
-          "author",
-          "pag",
-          "publicacion",
-          "editorial",
-          "ISBN",
-          "genero",
-          "comentarios",
-          "edicion",
-        ],
-        properties: {
-          _id: { bsonType: "double", description: "PK id becomes _id " },
-          nombre: { bsonType: "string" },
-          author: { bsonType: "string" },
-          pag: { bsonType: "double" },
-          publicacion: { bsonType: "date" },
-          editorial: { bsonType: "string" },
-          ISBN: { bsonType: "string" },
-          genero: { bsonType: "string" },
-          comentarios: { bsonType: "string" },
-          edicion: { bsonType: "int" },
-        },
-      },
-      $expr: [],
-    });
-
-    db.createCollection("Estantes", {
-      $jsonSchema: {
-        bsonType: "object",
-        additionalProperties: false,
-        required: ["_id", "position", "categoria", "libro"],
-        properties: {
-          _id: { bsonType: "double", description: "PK id becomes _id " },
-          position: { bsonType: "double" },
-          categoria: { bsonType: "string" },
-          libro: { bsonType: "string" },
-        },
-      },
-      $expr: [],
-    });
-
-    res.send("Tables created")
-  } catch (e) {
-    res.send(e);
-  }
-}
-
 async function createLibro(req, res) {
   try {
     const {
@@ -85,7 +30,7 @@ async function createLibro(req, res) {
     };
 
     const result = await client
-      .db("libros")
+      .db("admin")
       .collection("libros")
       .insertOne(libro);
     res.send(result);
@@ -96,7 +41,7 @@ async function createLibro(req, res) {
 
 async function getLibros(req, res) {
   const result = await client
-    .db("libros")
+    .db("admin")
     .collection("libros")
     .find({})
     .toArray();
@@ -116,6 +61,8 @@ async function updateLibros(req, res) {
       genero,
       comentarios,
       edicion,
+      copias,
+      enEstante,
     } = req.body;
     const libro = {
       nombre,
@@ -127,11 +74,13 @@ async function updateLibros(req, res) {
       genero,
       comentarios,
       edicion,
+      copias,
+      enEstante
     };
     const result = await client
-      .db("libros")
+      .db("admin")
       .collection("libros")
-      .updateOne({ id: id }, { $set: libro });
+      .updateOne({ _id: id }, { $set: libro });
     res.status(200).send(result);
   } catch (e) {
     res.status(500).send(e);
@@ -142,7 +91,7 @@ async function deleteLibros(req, res) {
   try {
     const { id } = req.body;
     const result = await client
-      .db("libros")
+      .db("admin")
       .collection("libros")
       .deleteOne({ _id: id });
     res.status(200).send(result);
@@ -151,10 +100,72 @@ async function deleteLibros(req, res) {
   }
 }
 
+async function getLibroById(req, res ){
+  try{
+    const {id} = req.query;
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .findOne({ _id: parseInt(id) });
+    res.status(200).send({
+      message: "Libro encontrado",
+      data: result
+    });
+  }catch(e){
+    res.status(500).send({
+      message: e.message
+    })
+  }
+}
+
+async function createEstante(req, res){
+  try{
+    const { id, position, categoria } = req.body;
+    const result = await client.db("admin").collection("estantes").insertOne({
+      _id: id,
+      position,
+      categoria,
+      libros: []
+    })
+    res.status(200).send({
+      message: "Estante creado",
+      data: result
+    })
+  }catch(e){
+    res.status(500).send({
+      message: e.message
+    })
+  }
+}
+
+async function libroAEstante(req, res ){
+  try{
+    const { idEstante, idLibro } = req.body;
+    const result = await client
+      .db("admin")
+      .collection("libros")
+      .findOne({ _id: idLibro });
+    const resultEstante = await client
+      .db("admin")
+      .collection("estantes")
+      .updateOne({ _id: idEstante}, { $push: { libros: result } });
+    res.status(200).send({
+      message: "Libro agregado al estante",
+      data: resultEstante
+    })
+  }catch(e){
+    res.status(500).send({
+      message: e.message
+    })
+  }
+}
+
 module.exports = {
-  createTables,
   createLibro,
+  createEstante,
   getLibros,
   updateLibros,
   deleteLibros,
+  getLibroById,
+  libroAEstante
 };
--- routes/test-route.js
+++ routes/test-route.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const testController = require("../controllers/test-controller")
 
-router.post("/createTables", testController.createTables)
 router.post("/postLibros", testController.createLibro)
+router.post("/postEstante", testController.createEstante)
 
 router.get("/getLibros", testController.getLibros)
+router.get("/getLibrosById", testController.getLibroById)
 
 router.put("/updateLibros", testController.updateLibros)
+router.put("/addToEstante", testController.libroAEstante)
 
 router.delete("/deleteLibros", testController.deleteLibros)
 
